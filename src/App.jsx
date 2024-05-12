06d1d4cbe243ceb2141d7317a780188c2d094e55
useryesa9@gmail.com <useryesa9@gmail.com>
2024-05-12 14:51:52 +0700

    Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import React from "react"
 import Login from "./page/login";
 import Register from "./page/register";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>Hello word :)</h1>
+  },
+  
+  {
+    path: "/login",
+    element: <Login />,
+  },
+
+  {
+    path: "/register",
+    element: <Register />,
+  },
+])
 function App() {
 
   return (
     <>
-      <Register />
+      <RouterProvider router={router} />
     </>
 
   )
